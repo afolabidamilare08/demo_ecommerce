49e65c26056b6f2eb0bfa8c24d1db663bd796d48
Afolabi Damilare <afolabidamilare08@gmail.com>
2023-02-09 17:31:51 +0100

    frf

--- src/components/footer/footerComp.js
+++ src/components/footer/footerComp.js
@@ -8,112 +8,40 @@ import './footer.css'
 export const FooterComp = () => {
 
     return(
-
-        <div className="footer-div" >
-
-        <div className="footer-div-top" >
-            <a href="/" className="footer-div-top-img" >
-                    <img alt="" src={Logo} className="footer-div-top-img-img" />
-            </a>
-            {/* <a href="/" className="footer-div-top-link" >
-                <span className="footer-div-top-link-farmy" >Farmy</span>App
-            </a> */}
-        </div>
-
-        <div className="footer-div-others" >
-
-            <div className="footer-div-others-contact" >
-
-                <div className="footer-div-others-contact-top" >
-                    Follow Us On
-                </div>
-
-                <div className="footer-div-others-contact-mid" >
-                    
-                    <a href="#" className="footer-div-others-contact-mid-link" >
-
-                        <FaFacebookSquare className="footer-div-others-contact-mid-link-ic" />
-
-                        <span className="footer-div-others-contact-mid-link-txt" > Facebook </span>
-                    </a>
-
-                    <a href="#" className="footer-div-others-contact-mid-link" >
-
-                        <FaTwitterSquare className="footer-div-others-contact-mid-link-ic" />
-
-                        <span className="footer-div-others-contact-mid-link-txt" > Twitter </span>
-                    </a>
-
-                    <a href="#" className="footer-div-others-contact-mid-link" >
-
-                        <FaInstagramSquare className="footer-div-others-contact-mid-link-ic" />
-
-                        <span className="footer-div-others-contact-mid-link-txt" > Instagram </span>
-                    </a>
-
-                    <a href="#" className="footer-div-others-contact-mid-link" >
-
-                        <FaLinkedinIn className="footer-div-others-contact-mid-link-ic" />
-
-                        <span className="footer-div-others-contact-mid-link-txt" > Linkedin </span>
-                    </a>
-
-                </div>
-
+        <footer class="site-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 col-md-6">
+                <h6>About</h6>
+                <p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+              </div>
+    
+              <div class="col-xs-6 col-md-3">
+                <h6>Categories</h6>
+                <ul class="footer-links">
+                  <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
+                  <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
+                  <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
+                  <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
+                  <li><a href="http://scanfcode.com/category/android/">Android</a></li>
+                  <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+                </ul>
+              </div>
+    
+              <div class="col-xs-6 col-md-3">
+                <h6>Quick Links</h6>
+                <ul class="footer-links">
+                  <li><a href="http://scanfcode.com/about/">About Us</a></li>
+                  <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+                  <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
+                  <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+                  <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+                </ul>
+              </div>
             </div>
-
-            {/* <div className="footer-div-others-qlinks" >
-                <div className="footer-div-others-contact-top footer-div-others-qlinks-top" >
-                    Quick Links 
-                </div>
-                <div className="footer-div-others-qlinks-mid" >
-
-                    <a href="/" className="footer-div-others-qlinks-mid-link" >
-                        <BsHouse className="footer-div-others-qlinks-mid-link-ic" />
-                        Home
-                    </a>
-
-                    <a href="/profile" className="footer-div-others-qlinks-mid-link" >
-                        <FiUser className="footer-div-others-qlinks-mid-link-ic" />
-                        Profile
-                    </a>
-
-                    <a href="/about_referral" className="footer-div-others-qlinks-mid-link" >
-                        <BsGift className="footer-div-others-qlinks-mid-link-ic" />
-                        Referral
-                    </a>
-
-                    <a href="/wholesale" className="footer-div-others-qlinks-mid-link" >
-                        <AiFillShop className="footer-div-others-qlinks-mid-link-ic" />
-                        Farmers Market
-                    </a>
-
-                </div>
-            </div>
-
-            <div className="footer-div-others-deli" >
-                <div className="footer-div-others-deli-top footer-div-others-contact-top" >
-                    our Delivery services  
-                </div>
-                <div className="footer-div-others-deli-num" >
-                    Please Note That food delivery is only available in Ibadan.
-                </div>
-            </div>
-
-            <div className="footer-div-others-deli" >
-                <div className="footer-div-others-deli-top footer-div-others-contact-top" >
-                    Contact us  
-                </div>
-                <div className="footer-div-others-deli-num" >
-                    07042995949
-                    <br/>
-                    info@farmyapp.com
-                </div>
-            </div> */}
-
-        </div>
-
-    </div>
+            {/* <hr/> */}
+          </div>
+    </footer>
 
     );
 
