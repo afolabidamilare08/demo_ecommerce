3be8cdb79199e1a40e782771db96ff31dccda9e8
Afolabi Damilare <afolabidamilare08@gmail.com>
2023-02-15 13:49:52 +0100

    cs

--- src/components/footer/footerComp.js
+++ src/components/footer/footerComp.js
@@ -25,23 +25,21 @@ export const FooterComp = () => {
               <div class="col-xs-6 col-md-3">
                 <h6>Categories</h6>
                 <ul class="footer-links">
-                  <li><a href="http://scanfcode.com/category/c-language/">C</a></li>
-                  <li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
-                  <li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
-                  <li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
-                  <li><a href="http://scanfcode.com/category/android/">Android</a></li>
-                  <li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+                  <li>Shoes</li>
+                  <li>Bags</li>
+                  <li>Trousers</li>
+                  <li>Shirts</li>
+                  <li>Hats</li>
+                  <li>Track Pants</li>
                 </ul>
               </div>
     
               <div class="col-xs-6 col-md-3">
                 <h6>Quick Links</h6>
                 <ul class="footer-links">
-                  <li><a href="http://scanfcode.com/about/">About Us</a></li>
-                  <li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
-                  <li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
-                  <li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
-                  <li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+                  <li>About Us</li>
+                  <li>Contact Us</li>
+                  <li>Privacy Policy</li>
                 </ul>
               </div>
               
